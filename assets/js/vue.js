859bbfd38ea0985dab91cb8fec171f0d0b00d5f7
Geoffrey BÉDLÉ <geoffrey.bedle@gmail.com>
2021-03-23 00:43:46 +0100

    product block + cart

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -76,22 +76,84 @@ const products = [
 
 const Home = {
   template: "#home",
+  name: "Home",
   data: () => {
     return {
       products,
-      searchKey: '',
-    }
+      searchKey: "",
+      liked: [],
+      cart: [],
+    };
   },
   computed: {
-filteredList(){
-  return this.products.filter((product) => {
-    return product.description.toLowerCase().includes(this.searchKey.toLowerCase())
-  })
-}
+    filteredList() {
+      return this.products.filter((product) => {
+        return product.description
+          .toLowerCase()
+          .includes(this.searchKey.toLowerCase());
+      });
+    },
+    getLikeCookie(){
+     let cookieValue = JSON.parse($cookies.get('like'))
+     cookieValue == null ? this.liked = [] : this.liked = cookieValue
+    },
+    cartTotalAmount(){
+      let total = 0;
+      for(let item in this.cart){
+        total = total + this.cart[item].quantity * this.cart[item].price
+      }
+      return total;
+    },
+    itemTotalAmount(){
+      let itemTotal = 0;
+      for(let item in this.cart){
+        itemTotal = itemTotal + (this.cart[item].quantity);
+      }
+      return itemTotal
+    }
   },
-  name: "Home",
- 
-};
+  methods: {
+    setLikedCookie() {
+      document.addEventListener("input", () => {
+        setTimeout(() => {
+          $cookies.set("like", JSON.stringify(this.liked));
+        }, 300);
+      });
+    },
+    addToCart(product){
+      // check if already in array
+      for (let i = 0; i < this.cart.length; i++){
+        if(this.cart[i].id === product.id){
+          return this.cart[i].quantity++
+        }
+      }
+      this.cart.push({
+        id: product.id,
+        img: product.img,
+        description: product.description,
+        price: product.price,
+        quantity: 1
+      })
+    },
+    cartPlusOne(product){
+      product.quantity = product.quantity + 1;
+    },
+    cartMinusOne(product, id){
+      if(product.quantity === 1){
+        return this.cartRemoveItem(id)
+      }
+      product.quantity = product.quantity - 1;
+    },
+    cartRemoveItem(id){
+      this.$delete(this.cart,id)
+    }
+  },
+  mounted: function() {
+    console.log('mounted !')
+    this.getLikeCookie;
+  },
+}
+
 const UserSettings = {
   template: "<h1>User Settings</h1>",
   name: "UserSettings",
@@ -117,5 +179,4 @@ const router = new VueRouter({
 
 const vue = new Vue({
   router,
- 
 }).$mount("#app");
